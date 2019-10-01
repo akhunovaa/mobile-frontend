9023bd40751a292fba328e3bda188cf6c8407dc0
Azat Akhunov <azat.akhunov@protonmail.com>
2019-10-02 00:35:57 +0300

    [NF] project in new branch initialized

--- src/home/Home.js
+++ src/home/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
             <div>
                 <div id="main_menu">
                     <ul id="sub01">
-                        <li><a href="/">ГЛАВНАЯ СТРАНИЦА</a></li>
+                        <li><a href="/">ГЛАВНАЯ СТРАНИЦА </a></li>
                         <li><a href="mailto:admin@botmasterzzz.com">ОБРАТНАЯ СВЯЗЬ</a></li>
                         <li><a href="tg://resolve?domain=botmasterzzzrobot">ТЕЛЕГРАММ БОТ</a></li>
                     </ul>
