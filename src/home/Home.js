6f4bc5f05d72fcde8776b5761a162efe49e0facb
Azat Akhunov <azat.akhunov@protonmail.com>
2019-10-06 22:13:41 +0300

    [NF] login auth state added

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react';
 import './Home.css';
 import LoginForm from '../home/LoginForm';
-import {Redirect} from "react-router";
 
 class Home extends Component {
 
-    render() {
-        if(this.props.authenticated) {
-            return <Redirect
-                to={{
-                    pathname: "/dashboard",
-                    state: { from: this.props.location }
-                }}/>;
-        }else {
-            return (
-                <div className={"main"}>
-                    <LoginForm {...this.props} />
-                </div>
-            )
+    constructor(props) {
+
+        super(props);
+
+        if (this.props.currentUser) {
+            const fullName = this.props.currentUser.surname + ' ' + this.props.currentUser.name + ' ' + this.props.currentUser.patrName;
+            this.state.currentUser = {
+                name: fullName,
+                email: this.props.currentUser.email,
+                phone: this.props.currentUser.phone,
+                message: '',
+                captchaToken: ''
+            };
         }
     }
-}
 
+    render() {
+        if (!this.props.authenticated) {
+            return <div className={"main"}>
+                <LoginForm {...this.props} />
+            </div>
+        }
+
+        return (
+            <div className={"main"}>
+                <p>Hello, {this.state.currentUser.name}</p>
+            </div>
+        )
+    }
+}
 
 export default Home;
