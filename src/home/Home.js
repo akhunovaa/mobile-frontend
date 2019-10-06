92942ba97777014f78ad0aeea5f00eed666f59ec
Azat Akhunov <azat.akhunov@protonmail.com>
2019-10-06 23:24:10 +0300

    [NF] login auth state added

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,25 +1,12 @@
 import React, {Component} from 'react';
 import './Home.css';
 import {Header, Grid} from "semantic-ui-react";
-import {NavLink} from "react-router-dom";
 
 class Home extends Component {
 
     state = {};
 
     render() {
-
-        if(!this.props.authenticated) {
-            return <div className={"main"}>
-                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-                    <Grid.Column className="grid-column">
-                        <Header as='h2' color='teal' textAlign='center'>
-                            Для работы с данным порталом необходима <NavLink to="/login">авторизация</NavLink>
-                        </Header>
-                    </Grid.Column>
-                </Grid>
-            </div>
-        }
 
         return (
             <div className={"main"}>
